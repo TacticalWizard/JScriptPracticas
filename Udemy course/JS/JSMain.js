55b6ae2139488270cad9a1d474f0c775e56a0439
TacticalWizard <86961420+TacticalWizard@users.noreply.github.com>
2022-04-22 00:20:48 -0300

    Add files via upload

---
+++ Udemy course/JS/JSMain.js
@@ -0,0 +1,13 @@
+function changeText(id) {
+    id.innerHTML = "Nuevo texto";
+}
+
+function CalcularAños(form) {
+    form.EdadPerro.value = form.Edad.value * 7
+}
+
+//JavaScript output
+document.write(10 + 5);
+
+//innerHTML
+document.getElementById("coso").innerHTML = 10 + 10;
